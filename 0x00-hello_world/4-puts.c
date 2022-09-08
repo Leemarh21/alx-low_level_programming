77d42990caaf04b5e7c0a3c13ff1dde014bbfec1
Leemarh21 <teniolaoguns@gmail.com>
2022-09-08 11:19:49 -0700

    programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints
+ *
+ * Return: Always 0 (Was successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle');
+	return (0);
+}
