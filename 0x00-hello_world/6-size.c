985fca7585510bafc7b16eb21747d212ffb6b3d1
Leemarh21 <teniolaoguns@gmail.com>
2022-09-08 13:36:14 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like holding a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	char c;
 	int i;
