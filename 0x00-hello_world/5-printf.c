cdbba41dbdccc72b29340e17871babe250490063
Leemarh21 <teniolaoguns@gmail.com>
2022-09-08 11:42:55 -0700

    Printing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
- * Retuen: 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 ^C
