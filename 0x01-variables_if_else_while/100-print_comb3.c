d4cf2d01c9348950d9c5cee866b95e4170c5109e
Leemarh21 <teniolaoguns@gmail.com>
2022-09-09 19:14:51 -0700

    comb 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,22 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int i, j;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (j = 1; j <= 9; j++)
 		{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
+			if (j > i)
+	                {
+				putchar(i + '0');
+				putchar(j + '0');
 
-				if (digit1 == 8 && digit2 == 9)
-				continue;
+				if (i != 8)
 
 				putchar(',');
 				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
