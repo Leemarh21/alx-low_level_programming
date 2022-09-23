6c880b50aa2af59eaaa727b9926ff3043da924ce
Leemarh21 <teniolaoguns@gmail.com>
2022-09-23 13:08:59 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,32 +2,38 @@
 
 /**
  * cap_string - capitalizes all words in a string
- * @s: string to capitalize
+ * @str: string to capitalize
  *
  * Return: address of s
  */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
+	int index = 0;
 
-	while (*(s + i))
+	while (str[index])
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - i))
-						*(s + i) -= 'a' - 'A';
-				}
-			}
-		}
-		i++;
+		while(!(str[index] >= 'a' && str[index] <= 'a'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	return (s);
+
+	return (str);
 }
