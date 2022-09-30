be42867e97c0951213aafb4ffb4c864d7a3ffa29
Leemarh21 <teniolaoguns@gmail.com>
2022-09-30 14:42:08 -0700

    whats

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,9 +11,9 @@
  *
  * Return: return 0
  */
-int main(int argc, char *argv[j])
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
 	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
