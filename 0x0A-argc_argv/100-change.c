4f9fb81a8ef86d51b9bbb220bb777671d2c38abb
Leemarh21 <teniolaoguns@gmail.com>
2022-09-30 17:07:03 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,42 +10,40 @@
 
 int main(int argc, char **argv)
 {
-/*Declaring variables*/
-int position, total, change, aux;
-int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+	int c, coins = 0;
 
-position = total = change = aux = 0;
+	if (args != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	c = atoi(argv[1]);
+	if (c < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (; c >= 0;)
+	{
+		if (c >= 25)
+			c -= 25;
 
-if (argc != 1)
-{
-printf("Error\n");
-return (1);
+		else if (c >= 10)
+			c -= 10;
+		
+		else if (c >= 5)
+			c -= 5;
+
+		else if (c >= 2)
+			c -= 2;
+
+		else if (c >= 1)
+			c -= 1;
+		else
+			break;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+	return (0);
 }
 
-total = atoi(argv[1]); /*Covert str to int*/
-
-if (total <= 0)
-{
-printf("0\n");
-return (0);
-}
-
-/*Declaring while*/
-
-while (coins[position] != '\0')
-
-{
-if (total >= coins[position])
-{
-aux = (total / coins[position]);
-change += aux;
-total -= coins[position] * aux;
-}
-
-positions++;
-
-}
-
-printf("%d\n", change);
-return (0);
-}
