7004f9f3708d9a9635edd8cde0a28f57fb63e92a
Leemarh21 <teniolaoguns@gmail.com>
2022-10-07 14:28:58 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr = NULL)
+	if (ptr == NULL)
 	{
 		nptr = malloc(new_size);
 		if (nptr == NULL)
