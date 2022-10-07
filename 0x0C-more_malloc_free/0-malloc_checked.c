c8a585a0402181531ba755d0d9d7c3edf39dc210
Leemarh21 <teniolaoguns@gmail.com>
2022-10-07 03:59:35 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,13 +7,10 @@
 
 void *malloc_checked(unsigned int b);
 {
-void *p;
+	void *p = malloc(b);
 
-p = malloc(b);
-if (p == NULL)
-{
-exit(98);
-}
+	if (p == NULL)
+		exit(98);
 
-return (p);
+	return (p);
 }
