8ba4c561029b4a840ea506402d1af61cd8760486
Leemarh21 <teniolaoguns@gmail.com>
2022-10-11 01:41:37 -0700

    print dpg

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,25 +5,28 @@
 /**
  * print_dog - prints a struct dog
  * @d: struct dog to be printed
+ *
+ * Return: void
  */
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d == 0)
+	{
 		return;
-
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
+	}
 	else
-		printf("Name: %s\n", d->name);
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 
-	if (d->age < 0)
-		printf("Age: (nil)\n");
-	else
 		printf("Age: %f\n", d->age);
 
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n". d->owner);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
