e20d118630d77480522ee7b359a7c676ef76064d
Leemarh21 <teniolaoguns@gmail.com>
2022-10-25 14:57:13 -0700

    100 reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,22 +8,20 @@
  */
 listint_t *reverse_listint(listint_t **head);
 {
-	listint_t *ahead, *behind;
+	listint_t *p;
+	listint_t 8n;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
+	p = NULL;
+	n = NULL;
 
-	behind = NULL;
-
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
-		ahead = (*head)->next;
-		(*head)->next = behind;
-		behind = *head;
-		*head = ahead;
+		n = (*head)->next;
+		(*head)->next =p;
+		p = *head;
+		*head = n;
 	}
 
-	(*head)->next = behind;
-
+	*head = p;
 	return (*head);
 }
